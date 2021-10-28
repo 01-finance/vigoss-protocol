aad2ddafd6cfde6e8719c8426bcea747fd49c134
AceLuodan <903585177@qq.com>
2021-10-28 10:42:18 +0800

    add-batch-vigoss

---
+++ migrations/18_batchvigoss.js
@@ -0,0 +1,18 @@
+
+const BatchVigoss = artifacts.require("BatchVigoss");
+
+const { writeAbis } = require('./log');
+
+module.exports = async function(deployer, network, accounts) {
+
+  let vgsForMargin = require(`../front/abis/VGSForMargin.${network}.json`);
+  let vgsForLP = require(`../front/abis/VGSForLP.${network}.json`);
+
+  console.log(accounts[0]);
+  await deployer.deploy(BatchVigoss);
+
+    await writeAbis(BatchVigoss, 'BatchVigoss', network);
+
+    console.log(accounts[0]);
+
+}
